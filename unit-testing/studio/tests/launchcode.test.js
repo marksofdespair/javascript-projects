7e1bce12b854307b6f5002f76efb070438546fc8
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-10-10 12:45:04 -0500

    test studio progress ig

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,31 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+    test("Checks testing launchcode", function(){
+      expect(launchcode.organization).toBe("nonprofit");
+    });
+    
+    test("checks executive director", function(){
+      expect(launchcode.executiveDirector).toBe("Jeff");
+    });
+
+    test("checks percentageCoolEmployees", function (){
+      expect(launchcode.percentageCoolEmployees).toBe(100);
+    });
+
+    test("checks programsOffered", function (){
+      expect(launchcode.programsOffered).toContain("Web Development")
+      expect(launchcode.programsOffered).toContain("Data Analysis")
+      expect(launchcode.programsOffered).toContain("Liftoff")
+      expect(launchcode.programsOffered.length).toBe(3)
+    });
 });
+
+describe("Testing launchOutput()", function(){
+
+  test("should output Launch! when passed a number only divisible by 2", function (){
+    let divisibleby2 = 4;
+    let result = launchOutput(divisibleby2);
+    expect(result).toBe('Launch!');
+  });
+})
